84be5f71315a4c4067e4df3fc44cf706a9943bdc
Son Hoai Tran <son.tran@exoplatform.com>
2011-03-24 07:47:55 +0000

    MOB-216: Load Configuration informations from a Singleton

---
+++ Sources/Shared/Data Manager/Proxies/Configuration.h
@@ -0,0 +1,37 @@
+//
+//  Configuration.h
+//  eXoPlatform
+//
+//  Created by Tran Hoai Son on 3/21/11.
+//  Copyright 2011 home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CXMLDocument.h"
+#import "CXMLNode.h"
+#import "CXMLElement.h"
+
+@interface ServerObj : NSObject {
+    NSString*   _strServerName;
+    NSString*   _strServerUrl;
+    BOOL        _bSystemServer;
+}
+@property BOOL _bSystemServer;
+@property (nonatomic, retain) NSString* _strServerName;
+@property (nonatomic, retain) NSString* _strServerUrl;
+@end
+
+//======================================================================
+@interface Configuration : NSObject {
+    NSMutableArray*        _arrServerList;
+}
+
++ (Configuration*)sharedInstance;
+- (NSMutableArray*)getServerList;
+- (CXMLNode*) getNode: (CXMLNode*) element withName: (NSString*) name;
+- (NSString*) getNodeValue: (CXMLNode*) node withName: (NSString*) name;
+- (NSMutableArray*)parseConfiguration:(NSData*)data withBSystemSever:(BOOL)bSystemServer;
+- (NSData*)createXmlDataWithServerList:(NSMutableArray*)arrServerList;
+- (NSData*)readFileWithName:(NSString*)strFileName;
+- (void)writeData:(NSData*)data toFile:(NSString*)strFileName;
+@end
