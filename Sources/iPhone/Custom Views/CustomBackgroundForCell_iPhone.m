cc4667d419b6c8646ed872c1bfff7b431e2fe6aa
stevan_lemeur <stevan_lemeur>
2011-05-30 14:44:07 +0000

    MOB-300 : Give a selectedBackgroundView to Standard UITableView (eXo)

--- Sources/iPhone/Custom Views/CustomBackgroundForCell_iPhone.m
+++ Sources/iPhone/Custom Views/CustomBackgroundForCell_iPhone.m
@@ -52,32 +52,50 @@
     //Create an image streachable images for background
     UIImage *imgBgNormal;
     
+    UIImage *imgBgSelected;
+    
     if (self.position == CustomCellBackgroundViewPositionTop) {
         //If the position is top
         imgBgNormal = [[UIImage imageNamed:@"CustomBackgroundForCellTop.png"]
                        stretchableImageWithLeftCapWidth:5 topCapHeight:10];
+        imgBgSelected = [[UIImage imageNamed:@"CustomBackgroundForCellTopSelected.png"]
+                       stretchableImageWithLeftCapWidth:5 topCapHeight:10];
+        
     } else if (self.position == CustomCellBackgroundViewPositionBottom) {
         //If the position is bottom
         imgBgNormal = [[UIImage imageNamed:@"CustomBackgroundForCellBottom.png"]
                        stretchableImageWithLeftCapWidth:5 topCapHeight:10];
+        imgBgSelected = [[UIImage imageNamed:@"CustomBackgroundForCellBottomSelected.png"]
+                         stretchableImageWithLeftCapWidth:5 topCapHeight:10];
 	
     } else if (self.position == CustomCellBackgroundViewPositionMiddle) {
         //If the position is Middle
         imgBgNormal = [[UIImage imageNamed:@"CustomBackgroundForCellMiddle.png"]
                        stretchableImageWithLeftCapWidth:5 topCapHeight:10];
+        imgBgSelected = [[UIImage imageNamed:@"CustomBackgroundForCellMiddleSelected.png"]
+                         stretchableImageWithLeftCapWidth:5 topCapHeight:10];
 	
     } else if (self.position == CustomCellBackgroundViewPositionSingle) {
         //If the position is Single
         imgBgNormal = [[UIImage imageNamed:@"CustomBackgroundForCellSingle.png"]
                        stretchableImageWithLeftCapWidth:6 topCapHeight:10];
+        imgBgSelected = [[UIImage imageNamed:@"CustomBackgroundForCellSingleSelected.png"]
+                         stretchableImageWithLeftCapWidth:5 topCapHeight:10];
     
     }
     
     //Add images to imageView for the backgroundview of the cell
     UIImageView *imgVCellBGNormal = [[UIImageView alloc] initWithImage:imgBgNormal];
     
+    UIImageView *imgVCellBGSelected = [[UIImageView alloc] initWithImage:imgBgSelected];
+    
+    
     [self setBackgroundView:imgVCellBGNormal];
+    [self setSelectedBackgroundView:imgVCellBGSelected];
+
+    
     [imgVCellBGNormal release];
+    [imgVCellBGSelected release];
     
 }
 
