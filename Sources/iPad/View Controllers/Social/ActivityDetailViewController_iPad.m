fcf43de62385bf91f163cb33fbdff2449b98e857
Son Hoai Tran <son.tran@exoplatform.com>
2011-07-04 10:45:15 +0000

    MOB-317:  [iOS] Create the message editor

---
+++ Sources/iPad/View Controllers/Social/ActivityDetailViewController_iPad.m
@@ -0,0 +1,31 @@
+//
+//  ActivityDetailViewController_iPad.m
+//  eXo Platform
+//
+//  Created by Tran Hoai Son on 7/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ActivityDetailViewController_iPad.h"
+#import "MessageComposerViewController.h"
+#import "AppDelegate_iPad.h"
+#import "RootViewController.h"
+
+@implementation ActivityDetailViewController_iPad
+
+- (void)onBtnMessageComposer
+{
+    MessageComposerViewController*  messageComposerViewController;
+    
+    messageComposerViewController = [[MessageComposerViewController alloc] initWithNibName:@"MessageComposerViewController" bundle:nil];
+    
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:messageComposerViewController];
+    [messageComposerViewController release];
+    
+    navController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+    navController.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+    navController.modalPresentationStyle = UIModalPresentationFormSheet;
+    [[AppDelegate_iPad instance].rootViewController.menuViewController presentModalViewController:navController animated:YES];
+}
+
+@end
