7d24ee645507198576075d2a20fcbd79af48d03d
Son Hoai Tran <son.tran@exoplatform.com>
2011-07-05 09:16:11 +0000

    MOB-317: [iOS] create message editor

---
+++ Sources/iPad/View Controllers/Social/MessageComposerViewController_iPad.h
@@ -0,0 +1,16 @@
+//
+//  MessageComposerViewController_iPad.h
+//  eXo Platform
+//
+//  Created by Tran Hoai Son on 7/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MessageComposerViewController.h"
+
+@interface MessageComposerViewController_iPad : MessageComposerViewController {
+    
+}
+
+@end
