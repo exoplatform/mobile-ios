eef0528af625fd4aa3266ab3cf00b2d477183264
Son Hoai Tran <son.tran@exoplatform.com>
2011-07-15 06:50:04 +0000

    MOB-323: [iOS] Social Datamanager : Create the ActivityStreamProxy using RestKit (it was used to archive activity stream data)

--- Sources/Shared/Data Manager/Model/Social/SocialActivityStream.h
+++ Sources/Shared/Data Manager/Model/Social/SocialActivityStream.h
@@ -11,20 +11,35 @@
 
 
 @interface SocialActivityStream : RKObject {
-    NSString*           _identityId;
-    NSString*           _totalNumberOfComments;
-    NSString*           _liked;
-    NSString*           _postedTime;
-    NSString*           _title;
-    NSString*           _createdAt;
+    NSString*               _identityId;
+    NSString*               _totalNumberOfComments;
+    NSString*               _liked;
+    NSString*               _postedTime;
+    NSString*               _type;
+    NSString*               _posterIdentity;
+    NSString*               _activityStream;
+    NSString*               _identify;
+    NSString*               _title;
+    NSString*               _priority;
+    NSString*               _createdAt;
+    NSString*               _likedByIdentities;
+    NSString*               _titleId;
+    NSString*               _comments;
 }
 
-@property (nonatomic, retain) NSString* _identityId;
-@property (nonatomic, retain) NSString* _totalNumberOfComments;
-@property (nonatomic, retain) NSString* _liked;
-@property (nonatomic, retain) NSString* _postedTime;
-@property (nonatomic, retain) NSString* _title;
-@property (nonatomic, retain) NSString* _createdAt;
-
+@property (nonatomic, retain) NSString* identityId;
+@property (nonatomic, retain) NSString* totalNumberOfComments;
+@property (nonatomic, retain) NSString* liked;
+@property (nonatomic, retain) NSString* postedTime;
+@property (nonatomic, retain) NSString* type;
+@property (nonatomic, retain) NSString* posterIdentity;
+@property (nonatomic, retain) NSString* activityStream;
+@property (nonatomic, retain) NSString* identify;
+@property (nonatomic, retain) NSString* title;
+@property (nonatomic, retain) NSString* priority;
+@property (nonatomic, retain) NSString* createdAt;
+@property (nonatomic, retain) NSString* likedByIdentities;
+@property (nonatomic, retain) NSString* titleId;
+@property (nonatomic, retain) NSString* comments;
 
 @end
--- Sources/Shared/Data Manager/Model/Social/SocialActivityStream.m
+++ Sources/Shared/Data Manager/Model/Social/SocialActivityStream.m
@@ -11,20 +11,46 @@
 
 @implementation SocialActivityStream
 
-@synthesize _identityId;
-@synthesize _totalNumberOfComments;
-@synthesize _liked;
-@synthesize _postedTime;
-@synthesize _title;
-@synthesize _createdAt;
+@synthesize identityId = _identityId;
+@synthesize totalNumberOfComments = _totalNumberOfComments;
+@synthesize liked = _liked;
+@synthesize postedTime = _postedTime;
+@synthesize type = _type;
+@synthesize posterIdentity = _posterIdentity;
+@synthesize activityStream = _activityStream;
+@synthesize identify = _identify;
+@synthesize title = _title;
+@synthesize priority = _priority;
+@synthesize createdAt = _createdAt;
+@synthesize likedByIdentities = _likedByIdentities;
+@synthesize titleId = _titleId;
+@synthesize comments = _comments;
 
 
-+ (NSDictionary*)elementToPropertyMappings {
++ (NSDictionary*)relationshipToPrimaryKeyPropertyMappings {
     //for test
-	return [NSDictionary dictionaryWithObjectsAndKeys:nil];
-            //@"_identityId",@"identityId",nil];
+    return [NSDictionary dictionaryWithObjectsAndKeys:
+            @"identityId",@"identityId",
+//            @"totalNumberOfComments",@"activities.totalNumberOfComments",
+//            @"liked",@"activities.liked",
+//            @"postedTime",@"activities.postedTime",            
+//            @"type",@"activities.type",
+//            @"posterIdentity",@"activities.posterIdentity",
+//            @"activityStream",@"activities.activityStream",
+//            @"identify",@"activities.id",
+//            @"title",@"activities.title",
+//            @"priority",@"activities.priority",
+//            @"createdAt",@"activities.createdAt",
+//            @"likedByIdentities",@"activities.likedByIdentities",
+//            @"titleId",@"activities.titleId",
+//            @"comments",@"activities.comments",
+			nil];
 }
 
+//+ (NSArray*)relationshipsToSerialize
+//{
+//    return [NSArray array];
+//}
 
 - (void)dealloc {
     [_identityId release];
