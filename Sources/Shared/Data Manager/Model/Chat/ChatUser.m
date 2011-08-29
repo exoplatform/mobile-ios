10f67263962fb465485570b7b2d4db24abbcf316
Xuyen Mai <xuyen.mai@exoplatform.com>
2011-08-29 11:25:24 +0000

    MOB-472: Add chat's model files

---
+++ Sources/Shared/Data Manager/Model/Chat/ChatUser.m
@@ -0,0 +1,16 @@
+//
+//  ChatUser.m
+//  eXo Platform
+//
+//  Created by Mai Gia on 8/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ChatUser.h"
+
+
+@implementation ChatUser
+
+@synthesize user = _user;
+
+@end
---
+++ Sources/Shared/Data Manager/Model/Chat/eXoMessage.m
@@ -0,0 +1,111 @@
+//
+//  eXoMessage.m
+//  eXoApp
+//
+//  Created by Tran Hoai Son on 5/21/09.
+//  Copyright 2009 EXO-Platform. All rights reserved.
+//
+
+#import "eXoMessage.h"
+#import "defines.h"
+
+@implementation eXoMessage
+
+@synthesize statusId, name, screenName, text, icon, timestamp, replyType, status, source, favorited;
+
+- (void) dealloc {
+    [statusId release];
+    statusId = nil;
+    
+    [name release];
+    name = nil;
+    
+    [screenName release];
+    screenName = nil;
+    
+    [text release];
+    text = nil;
+    
+    [icon release];
+    icon = nil;
+    
+    [timestamp release];
+    timestamp = nil;
+    
+    [source release];
+    source= nil;
+    
+	[iconUpdateDelegate release];
+    iconUpdateDelegate = nil;
+    
+    [super dealloc];
+}
+
+- (BOOL) isEqual:(id)anObject 
+{
+    if ([[self statusId] isEqual:[anObject statusId]]) 
+	{
+        return TRUE;
+    }
+    return FALSE;
+}
+
+- (void) setStatus:(enum EXOMessageStatus)value 
+{
+    status = value;
+}
+
+- (BOOL) isReplyToMe 
+{
+	NSString *userName = [[NSUserDefaults standardUserDefaults] objectForKey:EXO_PREFERENCE_USERNAME];
+    if ([text hasPrefix:[@"@" stringByAppendingString:userName]]) 
+	{
+        return TRUE;
+    }
+    return FALSE;
+}
+
+- (BOOL) isProbablyReplyToMe 
+{
+	NSString *userName = [[NSUserDefaults standardUserDefaults] objectForKey:EXO_PREFERENCE_USERNAME];
+    NSString *query = [@"@" stringByAppendingString:userName];
+    NSRange range = [text rangeOfString:query];
+    
+    if (range.location != NSNotFound) 
+	{
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+- (BOOL) isMyUpdate 
+{
+	NSString *userName = [[NSUserDefaults standardUserDefaults] objectForKey:EXO_PREFERENCE_USERNAME];
+    return [screenName isEqualToString:userName];
+}
+
+- (void) finishedToSetProperties:(BOOL)forDirectMessage 
+{
+	if (forDirectMessage) 
+	{
+		replyType = EXO_MESSAGE_REPLY_TYPE_DIRECT;
+	} 
+	else 
+	{
+		if ([self isReplyToMe]) 
+		{
+		 replyType = EXO_MESSAGE_REPLY_TYPE_REPLY;
+		} 
+		else if ([self isProbablyReplyToMe]) 
+		{
+		 replyType = EXO_MESSAGE_REPLY_TYPE_REPLY_PROBABLE;
+		} 
+		else 
+		{
+		 replyType = EXO_MESSAGE_REPLY_TYPE_NORMAL;
+		}
+	}
+}
+
+@end
