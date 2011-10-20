10db0988c6646d69371dd9a88612fbaf64d03438
Trung Nguyen <trung.nguyenkhac@exoplatform.com>
2011-10-20 10:44:30 +0000

    MOB-671:[iPad] Start the application directly on ActivityStream

--- Sources/iPad/View Controllers/CascadeViewController/RootViewController.m
+++ Sources/iPad/View Controllers/CascadeViewController/RootViewController.m
@@ -99,7 +99,7 @@
     //rightSlideView.backgroundColor = [UIColor colorWithRed:242./255 green:242./255 blue:242./255 alpha:1.];
     rightSlideView.backgroundColor = [UIColor clearColor];
     
-    
+    [menuViewController tableView:menuViewController.tableView didSelectRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
     //Add the background image when no content
     UIImage *imageBg;
     CGRect frameForBgImage;
