c125927595b6621fd17b09221030fc8a69d7f710
Trung Nguyen <trung.nguyenkhac@exoplatform.com>
2011-11-01 04:35:55 +0000

    MOB-687:

--- Sources/Shared/UI/Social/ActivityForumDetailMessageTableViewCell.m
+++ Sources/Shared/UI/Social/ActivityForumDetailMessageTableViewCell.m
@@ -19,7 +19,22 @@
 
 - (void)setSocialActivityDetail:(SocialActivityDetails*)socialActivityDetail
 {
+    [super setSocialActivityDetail:socialActivityDetail];
     switch (_activityType) {
+        case ACTIVITY_WIKI_ADD_PAGE:{
+            [_webViewForContent loadHTMLString:
+             [NSString stringWithFormat:@"<html><head><style>body{background-color:transparent;color:#808080;font-family:\"Helvetica\";font-size:13;word-wrap: break-word;} a:link{color: #0888D6; text-decoration: none; font-weight: bold;}</style> </head><a  href=\"%@\">%@</a> %@<a href=\"%@\"> %@</a></body></html>", socialActivityDetail.posterIdentity.fullName, socialActivityDetail.posterIdentity.fullName, Localize(@"CreateWiki"), [_templateParams valueForKey:@"page_url"],[_templateParams valueForKey:@"page_name"]] 
+                                       baseURL:[NSURL URLWithString:[[NSUserDefaults standardUserDefaults] valueForKey:EXO_PREFERENCE_DOMAIN]]
+             ];
+        }
+            break;
+        case ACTIVITY_WIKI_MODIFY_PAGE:{
+            [_webViewForContent loadHTMLString:
+             [NSString stringWithFormat:@"<html><head><style>body{background-color:transparent;color:#808080;font-family:\"Helvetica\";font-size:13;word-wrap: break-word;} a:link{color: #0888D6; text-decoration: none; font-weight: bold;}</style> </head><a  href=\"%@\">%@</a> %@<a href=\"%@\"> %@</a></body></html>", socialActivityDetail.posterIdentity.fullName, socialActivityDetail.posterIdentity.fullName, Localize(@"EditWiki"), [_templateParams valueForKey:@"page_url"],[_templateParams valueForKey:@"page_name"]] 
+                                       baseURL:[NSURL URLWithString:[[NSUserDefaults standardUserDefaults] valueForKey:EXO_PREFERENCE_DOMAIN]]
+             ];
+        }
+            break;
         case ACTIVITY_FORUM_CREATE_TOPIC:{
             [_webViewForContent loadHTMLString:
              [NSString stringWithFormat:@"<html><head><style>body{background-color:transparent;color:#808080;font-family:\"Helvetica\";font-size:13;word-wrap: break-word;} a:link{color: #0888D6; text-decoration: none; font-weight: bold;}</style> </head><a  href=\"%@\">%@</a> %@<a href=\"%@\"> %@</a></body></html>", socialActivityDetail.posterIdentity.fullName, socialActivityDetail.posterIdentity.fullName, Localize(@"NewTopic"), [_templateParams valueForKey:@"TopicLink"],[_templateParams valueForKey:@"TopicName"]] 
@@ -63,9 +78,11 @@
         }
             break;
     }
-    _lbMessage.text = socialActivityDetail.title;
-    _lbDate.text = [socialActivityDetail.postedTimeInWords copy];
-    _imgvAvatar.imageURL = [NSURL URLWithString:socialActivityDetail.posterIdentity.avatarUrl];
+    if([_templateParams valueForKey:@"page_exceprt"] == nil){
+        _lbMessage.text = socialActivityDetail.title;
+    } else {
+        _lbMessage.text = [_templateParams valueForKey:@"page_exceprt"];
+    }
     
 }
 
