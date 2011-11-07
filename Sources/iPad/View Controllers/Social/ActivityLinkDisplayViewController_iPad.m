156817f53f70001bc5007d1903a6c9db8e02ad09
stevan_lemeur <stevan_lemeur>
2011-11-07 22:13:03 +0000

    MOB-714 : Manage larger content for landscape into activityDisplayLink

--- Sources/iPad/View Controllers/Social/ActivityLinkDisplayViewController_iPad.m
+++ Sources/iPad/View Controllers/Social/ActivityLinkDisplayViewController_iPad.m
@@ -8,8 +8,26 @@
 
 #import "ActivityLinkDisplayViewController_iPad.h"
 
+#define WIDTH_DIFF_BETWEEN_LANDSCAPE_AND_PORTRAIT 250
+
+
 
 @implementation ActivityLinkDisplayViewController_iPad
+
+-(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        
+        //If the orientation is in Landscape mode
+        if (UIInterfaceOrientationIsLandscape(self.interfaceOrientation)) {
+            CGRect tmpFrame = self.view.frame;
+            tmpFrame.size.width += WIDTH_DIFF_BETWEEN_LANDSCAPE_AND_PORTRAIT;
+            self.view.frame = tmpFrame;
+        }
+    }
+    return self;
+}
+
 
 
 - (void)dealloc
@@ -20,5 +38,29 @@
 - (void)setHudPosition {
     _hudDocument.center = CGPointMake(self.view.frame.size.width/2, (self.view.frame.size.height/2)-70);
 }
+
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Overriden to allow any orientation.
+    return YES;
+}
+
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation duration:(NSTimeInterval)duration
+{
+    if (UIInterfaceOrientationIsLandscape(interfaceOrientation)) { 
+        CGRect tmpRect = self.view.frame;
+        tmpRect.size.width += WIDTH_DIFF_BETWEEN_LANDSCAPE_AND_PORTRAIT;
+        tmpRect.origin.x -= WIDTH_DIFF_BETWEEN_LANDSCAPE_AND_PORTRAIT;
+        self.view.frame = tmpRect;
+    } else {
+        CGRect tmpRect = self.view.frame;
+        tmpRect.size.width -= WIDTH_DIFF_BETWEEN_LANDSCAPE_AND_PORTRAIT;
+        tmpRect.origin.x += WIDTH_DIFF_BETWEEN_LANDSCAPE_AND_PORTRAIT;
+        self.view.frame = tmpRect;
+    }
+}
+
 
 @end
