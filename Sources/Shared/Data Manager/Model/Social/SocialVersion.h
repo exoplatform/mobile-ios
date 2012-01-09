f6f74e1306027f2f2b755245ec7362101ae5a70d
Trung Nguyen <trung.nguyenkhac@exoplatform.com>
2012-01-09 03:22:31 +0000

    MOB-1005

---
+++ Sources/Shared/Data Manager/Model/Social/SocialVersion.h
@@ -0,0 +1,17 @@
+//
+//  SocialVersion.h
+//  eXo Platform
+//
+//  Created by Nguyen Khac Trung on 12/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SocialVersion : NSObject{
+    NSString * _version;
+    
+}
+@property (retain) NSString * version;
+
+@end
