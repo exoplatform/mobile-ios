5f0c2db0b81e3f90c06750d1a75f32894ece1729
Nguyen Quoc Viet <vietnq@exoplatform.com>
2013-05-13 09:59:33 +0700

    MOB-1461 [iOS6][iPad] wrong ui of activity detail view

--- Sources/iPad/View Controllers/Social/ActivityDetailViewController_iPad.m
+++ Sources/iPad/View Controllers/Social/ActivityDetailViewController_iPad.m
@@ -22,7 +22,7 @@
 #import "RoundRectView.h"
 
 #define kMinimumAdvancedInfoCellHeight 400.0
-
+#define kAdvancedInfoCellHeight 760.0
 @implementation ActivityDetailViewController_iPad
 
 @synthesize extraActionsCell = _extraActionsCell;
@@ -246,6 +246,9 @@
     if (indexPath.section == 1) {
         return self.extraActionsCell.frame.size.height;
     } else if (indexPath.section == 2) {
+        if([[[UIDevice currentDevice] systemVersion] floatValue] >= 6.0) { //workaround for MOB-1461
+            return kAdvancedInfoCellHeight;
+        }
         return self.advancedInfoController.view.frame.size.height;
     } else {
         return [super tableView:tableView heightForRowAtIndexPath:indexPath];
