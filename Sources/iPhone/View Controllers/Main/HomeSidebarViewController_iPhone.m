ea509714240022c5ea2b89d41e100d1111616b17
Ta Quan <quanrong1988@yahoo.com.vn>
2013-12-05 16:32:17 +0700

    MOB-1591 fix white arrow issue

--- Sources/iPhone/View Controllers/Main/HomeSidebarViewController_iPhone.m
+++ Sources/iPhone/View Controllers/Main/HomeSidebarViewController_iPhone.m
@@ -278,6 +278,7 @@
     if (rowType == eXoDocuments) {
         [self initAndSelectDocumentsViewController];
     }
+    [[UINavigationBar appearance] setBackIndicatorImage:nil];
     [self dismissModalViewControllerAnimated:YES];
 }
 
