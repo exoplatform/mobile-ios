a41002a59790781f7167f0ac230798dbf92be845
Philippe Aristote <paristote@exoplatform.com>
2014-05-08 11:40:27 +0700

    Fix license header to LGPL v3 and add license file in the project

--- Sources/Shared/UI/Dashboard/GadgetDisplayViewController.h
+++ Sources/Shared/UI/Dashboard/GadgetDisplayViewController.h
@@ -1,9 +1,20 @@
 //
-//  GadgetDisplayViewController.h
-//  eXo Platform
+// Copyright (C) 2003-2014 eXo Platform SAS.
 //
-//  Created by Stévan Le Meur on 25/08/11.
-//  Copyright 2011 eXo Platform. All rights reserved.
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of
+// the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this software; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+// 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 //
 
 #import <UIKit/UIKit.h>
--- Sources/iPad/View Controllers/CloudSignup/AlreadyAccountViewController_iPad.m
+++ Sources/iPad/View Controllers/CloudSignup/AlreadyAccountViewController_iPad.m
@@ -1,9 +1,20 @@
 //
-//  AlreadyAccountViewController_iPad.m
-//  eXo Platform
+// Copyright (C) 2003-2014 eXo Platform SAS.
 //
-//  Created by vietnq on 7/8/13.
-//  Copyright (c) 2013 eXoPlatform. All rights reserved.
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of
+// the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this software; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+// 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 //
 
 #import "AlreadyAccountViewController_iPad.h"
