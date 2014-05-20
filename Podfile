0f104afd838f72f760677ed332d87a633e07af18
Philippe Aristote <paristote@exoplatform.com>
2014-05-20 16:58:41 +0700

    MOB-1702 MOB-1032 Configure CocoaPods to add OHHTTPStub to test Rest Proxies

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '7.0'
+workspace 'eXo_Platform'
+xcodeproj 'eXo'
+   pod 'OHHTTPStubs'
---
+++ Tests/LoginProxyTestCase.m
@@ -0,0 +1,46 @@
+//
+// Copyright (C) 2003-2014 eXo Platform SAS.
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of
+// the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this software; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+// 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+//
+
+
+#import <XCTest/XCTest.h>
+
+@interface LoginProxyTestCase : XCTestCase
+
+@end
+
+@implementation LoginProxyTestCase
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
