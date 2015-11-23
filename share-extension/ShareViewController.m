01f9dae5ad3fe12fee348667e6528afc2d2566c8
diego10bk <toannm@exoplatform.com>
2015-11-23 18:16:07 +0700

    MOB-1988 Set MIMEType for item that upload by Share Extension

--- share-extension/ShareViewController.m
+++ share-extension/ShareViewController.m
@@ -890,14 +890,28 @@ NSMutableData * data;
         NSRange rangeOfDocLink = [fileURL rangeOfString:@"jcr"];
         NSString* docLink = [NSString stringWithFormat:@"/rest/%@", [fileURL substringFromIndex:rangeOfDocLink.location]];
         title = [NSString stringWithFormat:@"Shared a document <a href=\"%@\">%@</a>\"", docLink, fileName];
-        templateParams = @{
-                           @"DOCPATH":docPath,
-                           @"MESSAGE":message,
-                           @"DOCLINK":docLink,
-                           @"WORKSPACE":defaultWorkspace,
-                           @"REPOSITORY":currentRepository,
-                           @"DOCNAME":fileName
-                           };
+        NSString * mimeType = [self MIMETypeForFile:fileName];
+        if (mimeType && mimeType.length > 0) {
+            templateParams = @{
+                               @"DOCPATH":docPath,
+                               @"MESSAGE":message,
+                               @"DOCLINK":docLink,
+                               @"WORKSPACE":defaultWorkspace,
+                               @"REPOSITORY":currentRepository,
+                               @"DOCNAME":fileName,
+                               @"mimeType":mimeType
+                                   };
+        } else {
+            templateParams = @{
+                               @"DOCPATH":docPath,
+                               @"MESSAGE":message,
+                               @"DOCLINK":docLink,
+                               @"WORKSPACE":defaultWorkspace,
+                               @"REPOSITORY":currentRepository,
+                               @"DOCNAME":fileName
+                                   };
+
+        }
     }
     
     NSMutableDictionary *dictionary = [[NSMutableDictionary alloc] init];
@@ -1154,6 +1168,21 @@ NSMutableData * data;
     UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     return newImage;
+}
+
+-(NSString *) MIMETypeForFile:(NSString *)fileName {
+    NSRange lastPointRange = [fileName rangeOfString:@"." options:NSBackwardsSearch];
+    if (lastPointRange.location != NSNotFound) {
+        NSString * fileExtension = [fileName substringFromIndex:lastPointRange.location+1];
+        CFStringRef pathExtension = (__bridge CFStringRef)(fileExtension);
+        CFStringRef type = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension, NULL);
+        CFRelease(pathExtension);
+        // The UTI can be converted to a mime type:
+        NSString *mimeType = (__bridge_transfer NSString *)UTTypeCopyPreferredTagWithClass(type, kUTTagClassMIMEType);
+        return mimeType;
+    }
+    
+    return nil;
 }
 
 @end
